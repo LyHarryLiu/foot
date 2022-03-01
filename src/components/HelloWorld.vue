88caeeff42a6fc261f54e5bcd837b636e37fe81b
Liu <554769199@qq.com>
2022-03-01 16:16:38 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <img src="./../assets/欧豪.jpg" alt="" class="img">
+    <div>
+      吃什么: {{ msg }}
+    </div>
+    <el-radio-group v-model="value">
+      <el-radio :label="1">
+        点菜
+      </el-radio>
+      <el-radio :label="2">
+        不点菜
+      </el-radio>
+    </el-radio-group>
+    <el-button type="primary" @click="random" style="margin-left:10px">
+      点击出菜
+    </el-button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      arr: ["牛肉汤","水饺","米线","韩式拌饭","黄焖鸡米饭","红烧排骨饭","萝卜牛腩饭","剁椒拌饭","砂锅排骨","锅仔小酥肉","卤面","酸辣肚丝汤","大盘鸡盖浇饭","老洛阳烩菜","牛肉刀削面","重庆小面"],
+      arr2: ["干锅有机花菜","三色豆芽","蒜茸油菜","金瓜山药","肉沫蒸鸡蛋"],
+      arr3: ["杏鲍菇红烧肉","木须肉","奥尔良烤全翅"],
+      msg: '',
+      value: 1,
+    }
+  },
+  methods: {
+    random() {
+      if (this.value === 1) {
+        this.msg = `素菜: ${this.arr2[this.randomNum(this.arr2)]}和${this.arr2[this.randomNum(this.arr2)]} 荤菜: ${this.arr3[this.randomNum(this.arr3)]}和${this.arr3[this.randomNum(this.arr3)]}`
+      } else {
+        this.msg = this.arr[this.randomNum(this.arr)]
+      }
+    },
+    randomNum(len) {
+      return Math.floor( Math.random() * len.length )
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.img {
+  height: 500px;
 }
 </style>
